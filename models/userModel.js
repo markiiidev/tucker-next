3dfb0ab77d11ebd301f0ce301d522f20869772de
markiiidev <airmovesjoy29@gmail.com>
2021-08-30 15:24:07 +0800

    dev project

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    root: {
+        type: Boolean,
+        default: false
+    },
+    avatar: {
+        type: String,
+        default: 'https://res.cloudinary.com/ddpt9ntwz/image/upload/v1629901826/1_vqxkdt.png'
+    }
+}, {
+    timestamps: true
+})
+
+let Dataset = mongoose.models.user || mongoose.model('user', userSchema)
+export default Dataset
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {
+          "BASE_URL": "http://localhost:3000",
+          "MONGODB_URL": "mongodb+srv://dhel:dhel123@cluster0.e0v3w.mongodb.net/next_ecommerce?retryWrites=true&w=majority"
+  }
+}
